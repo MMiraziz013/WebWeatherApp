4ba631902d72fca7fda47a91f436b14ff189aec6
MMiraziz13==013 <aziz131104@gmail.com>
2024-11-06 23:55:13 +0500

    Addition of a way to integrate icons after a temperature. Fix of minor css issues.

--- Models/Weather.cs
+++ Models/Weather.cs
@@ -2,5 +2,9 @@
 {
     public class Weather
     {
+        public int Id { get; set; }
+        //public required string Name { get; set; } 
+        //public required string Wind { get; set; }
+        public string Humidity { get; set; }
     }
 }
